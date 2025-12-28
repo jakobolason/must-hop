96dd10d1d368a87a865a5b0103120f8c535ec9ec
Jakob Olason <jakobolason@proton.me>
2025-12-28 14:49:29 +0100

    trying to get logs to host machine

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,11 +8,19 @@
 #![deny(clippy::large_stack_frames)]
 
 // use esp_backtrace as _;
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp_hal::{Config, rmt::Rmt, time::Rate, timer::timg::TimerGroup};
+use esp_hal::{
+    Config,
+    gpio::AnyPin,
+    rmt::{ChannelCreator, Rmt},
+    time::Rate,
+    timer::timg::TimerGroup,
+};
 use esp_hal_smartled::{SmartLedsAdapterAsync, buffer_size_async};
 use panic_rtt_target as _;
+use rtt_target::rtt_init_default;
 use smart_leds::{
     RGB8, SmartLedsWriteAsync, brightness, gamma,
     hsv::{Hsv, hsv2rgb},
@@ -26,10 +34,10 @@ esp_bootloader_esp_idf::esp_app_desc!();
     reason = "it's not unusual to allocate larger buffers etc. in main"
 )]
 #[esp_rtos::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: Spawner) -> ! {
     let p = esp_hal::init(Config::default());
-    rtt_target::rtt_init_print!();
-
+    rtt_init_default!();
+    info!("Setting up peripherals ...");
     // for executor
     let timg0 = TimerGroup::new(p.TIMG0);
     let sw_interrup = esp_hal::interrupt::software::SoftwareInterruptControl::new(p.SW_INTERRUPT);
@@ -40,10 +48,24 @@ async fn main(_spawner: Spawner) -> ! {
     let rmt: Rmt<'_, esp_hal::Async> = Rmt::new(p.RMT, Rate::from_mhz(80))
         .expect("Failed to initialize RMT")
         .into_async();
-    let rmt_channel = rmt.channel0;
-    let mut rmt_buffer = [esp_hal::rmt::PulseCode::default(); buffer_size_async(1)];
+    spawner
+        .spawn(slide_rbg_colors(rmt.channel0, p.GPIO8.into()))
+        .expect("TASK slide_rbg_colors failed");
 
-    let mut led = SmartLedsAdapterAsync::new(rmt_channel, p.GPIO8, &mut rmt_buffer);
+    loop {
+        info!("Bing!");
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn slide_rbg_colors(
+    // mut rmt_channel: esp_hal::rmt::Channel<'static, esp_hal::Blocking, esp_hal::rmt::Tx>,
+    rmt_channel: ChannelCreator<'static, esp_hal::Async, 0>,
+    gpio8: AnyPin<'static>,
+) {
+    let mut rmt_buffer = [esp_hal::rmt::PulseCode::default(); buffer_size_async(1)];
+    let mut led = SmartLedsAdapterAsync::new(rmt_channel, gpio8, &mut rmt_buffer);
 
     let mut color = Hsv {
         hue: 0,
