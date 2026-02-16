4d3fae57d298752ad5612df941aa297ccac4f1e8
Jakob Olason <jakobolason@proton.me>
2026-02-16 16:02:16 +0100

    lora_task is so small that it shoulnt be a file

--- examples/lora/rak3272s/src/bin/main.rs
+++ examples/lora/rak3272s/src/bin/main.rs
@@ -5,8 +5,6 @@
 
 #[path = "../iv.rs"]
 mod iv;
-#[path = "../lora_tasks.rs"]
-mod lora_tasks;
 
 use defmt::{error, info};
 use embassy_executor::Spawner;
@@ -28,7 +26,17 @@ use lora_phy::sx126x::{Stm32wl, Sx126x};
 use {defmt_rtt as _, panic_probe as _};
 
 use self::iv::{InterruptHandler, Stm32wlInterfaceVariant, SubghzSpiDevice};
-use self::lora_tasks::{SensorData, lora_task};
+
+use embassy_stm32::spi::mode::Master;
+
+use embassy_stm32::mode::Async;
+use must_hop::tasks::lora;
+use postcard::to_slice;
+use {defmt_rtt as _, panic_probe as _};
+
+use serde::{Deserialize, Serialize};
+
+const LORA_FREQUENCY_IN_HZ: u32 = 868_100_000; // warning: set this appropriately for the region
 
 static CHANNEL: Channel<ThreadModeRawMutex, SensorData, 3> = Channel::new();
 
@@ -105,5 +113,43 @@ async fn sensor_task(
         info!("waiting {} seconds ...", r_num);
 
         Timer::after_secs(r_num).await;
+    }
+}
+
+const MAX_PACK_LEN: usize = 128;
+
+#[embassy_executor::task]
+pub async fn lora_task(
+    mut lora: Stm32wlLoRa<'static, Master>,
+    channel: channel::Receiver<'static, ThreadModeRawMutex, SensorData, 3>,
+) {
+    lora::lora_task(&mut lora, channel, LORA_FREQUENCY_IN_HZ).await;
+}
+
+// This creates the task which checks for sensor data
+// or someone trying to send data. This handles both receive and transmission logic
+type Stm32wlLoRa<'d, CM> = LoRa<
+    Sx126x<
+        iv::SubghzSpiDevice<Spi<'d, Async, CM>>,
+        iv::Stm32wlInterfaceVariant<Output<'d>>,
+        Stm32wl,
+    >,
+    Delay,
+>;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, defmt::Format, Copy, Clone)]
+pub struct SensorData {
+    pub device_id: u8,
+    pub temperate: f32,
+    pub voltage: f32,
+    pub acceleration_x: f32,
+}
+
+// TODO: Shuold not use this, only for prototyping
+impl From<SensorData> for [u8; MAX_PACK_LEN] {
+    fn from(data: SensorData) -> Self {
+        let mut buffer = [0u8; MAX_PACK_LEN];
+        to_slice(&data, &mut buffer).expect("Could not serialize sensor data");
+        buffer
     }
 }
