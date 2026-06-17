ffe72a67c1f5f522edad9f9ec234531f4657f0ae
Jakob Olason <jakobolason@proton.me>
2026-06-17 22:38:16 +0200

    long experiment

--- analysis/scripts/run_experiments.py
+++ analysis/scripts/run_experiments.py
@@ -15,11 +15,11 @@ from pathlib import Path
 from typing import Any
 
 # EXPERIMENT 1: 6 different PI controllers
-KPKIs = [(40, 50), (10, 50), (13, 32), (13, 5), (1, 1), (100, 500), (0, 0)]
-EXPERIMENTS = [
-    {"sf": 5, "bw": 125, "kp": kp, "ki": ki, "tau": 10, "times": 10}
-    for (kp, ki) in KPKIs
-]
+# KPKIs = [(40, 50), (10, 50), (13, 32), (13, 5), (1, 1), (100, 500), (0, 0)]
+# EXPERIMENTS = [
+#     {"sf": 5, "bw": 125, "kp": kp, "ki": ki, "tau": 10, "times": 10}
+#     for (kp, ki) in KPKIs
+# ]
 
 # EXPERIMENT 2: Test controller with different spreading factors
 # SFs = [5, 6, 7, 8, 9, 10, 11]
@@ -28,18 +28,20 @@ EXPERIMENTS = [
 # ]
 
 # EXPERIMENT 3: Test different heartbeat intervals
-# TAUS = [20, 25, 30]
-# EXPERIMENTS = [
-#     {
-#         "sf": 12,
-#         "bw": 125,
-#         "kp": int(4 / (tau / 100)),
-#         "ki": int(5 / (tau / 100)),
-#         "tau": tau,
-#         "times": 10,
-#     }
-#     for tau in TAUS
-# ]
+TAUS = [15, 20, 25, 30]
+KPKIs = [(4, 5), (2, 3)]
+EXPERIMENTS = [
+    {
+        "sf": 9,
+        "bw": 125,
+        "kp": int(kp / (tau / 100)),
+        "ki": int(ki / (tau / 100)),
+        "tau": tau,
+        "times": 1,
+    }
+    for tau in TAUS
+    for kp, ki in KPKIs
+]
 
 
 # Nodes: list of {"node_id": "<source id>", "probe_id": "<probe serial>"}
@@ -51,7 +53,7 @@ NODES = [
 
 
 # Seconds per experiment run (~10 min default)
-DURATION = 600
+DURATION = 7200
 
 # Seconds to wait between runs for radio settling
 INTER_RUN_DELAY = 30
